16160126b25af6043ab00b01062fa6c4c7cf7804
Nicholas Everton <nicholas.everton@hotmail.com>
2017-09-18 11:56:12 +1000

    Created some constructors and fields

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,13 @@
 
 public class Game {
 	
+	private Board board;
+	private Player[] players;
+	private Die die; // Move this to board?
 	
+	public Game() {
+		board = new Board();
+		die = new Die();
+	}
 	
 }
--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,12 @@
 
 public class Player {
 	
+	private String username;
+	private int id;
+	private Token[] tokens;
 	
+	public Player(String username) {
+		
+	}
 	
 }
