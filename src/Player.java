4d320116823afac273063beffdaa27ba81d74c90
Tony Tran <tony.tran@outlook.com.au>
2017-09-19 19:06:25 +1000

    simple getter/setter functions for Player class

--- src/Player.java
+++ src/Player.java
@@ -6,7 +6,32 @@ public class Player {
 	private Token[] tokens;
 	
 	public Player(String username) {
-		
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	// we can remove one of these depending on how we use Tokens in Board/Game
+	public void setTokens(Token[] tokens) {
+		this.tokens = tokens;
+	}
+	
+	public void setTokens(Token t1, Token t2, Token t3, Token t4) {
+		tokens = new Token[]{t1, t2, t3, t4};
+	}
+	
+	public Token[] getTokens() {
+		return tokens;
 	}
 	
 }
