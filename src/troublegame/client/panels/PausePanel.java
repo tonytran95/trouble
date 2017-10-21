2e7d80aef1be6aa73f946503442202a2c5c847a3
Michael <michael_lamyk@outlook.com>
2017-10-21 22:14:35 +1100

    Adapted pausemenu to theme

--- src/troublegame/client/panels/PausePanel.java
+++ src/troublegame/client/panels/PausePanel.java
@@ -1,6 +1,7 @@
 package troublegame.client.panels;
 
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -8,6 +9,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -31,6 +36,7 @@ public class PausePanel extends JPanel {
 	 * The swing user interface.
 	 */
 	private SwingUI swingUI;
+	private Image backgroundImage;
 	
 	/**
 	 * The constructor for the pause panel.
@@ -38,6 +44,15 @@ public class PausePanel extends JPanel {
 	 */
 	public PausePanel(SwingUI swingUI) {
 		this.swingUI = swingUI;
+		/**
+		 * set background image
+		 */
+		try {
+			backgroundImage = ImageIO.read(new File("./data/img/background3.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		this.init();
 	}
 	
@@ -45,17 +60,29 @@ public class PausePanel extends JPanel {
 	 * Initializes the start panel.
 	 */
 	public void init() {
-		Image image1 = Toolkit.getDefaultToolkit().getImage("./data/img/button_1.png");
-		Image image2 = Toolkit.getDefaultToolkit().getImage("./data/img/button_2.png");
-		Image newimg1 = image1.getScaledInstance(SwingUI.WIDTH, 200, Image.SCALE_SMOOTH);
-		Image newimg2 = image2.getScaledInstance(SwingUI.WIDTH, 200, Image.SCALE_SMOOTH);
+		setLayout(null);
+		Image image1 = Toolkit.getDefaultToolkit().getImage("./data/img/button_1s.png");
+		Image image2 = Toolkit.getDefaultToolkit().getImage("./data/img/button_2s.png");
+		Image newimg1 = image1.getScaledInstance(250, 55, Image.SCALE_SMOOTH);
+		Image newimg2 = image2.getScaledInstance(250, 55, Image.SCALE_SMOOTH);
 		ImageIcon imgIcon1 = new ImageIcon(newimg1);
 		ImageIcon imgIcon2 = new ImageIcon(newimg2);
 		
 		JButton returnGame = new JButton("Return to game");
-		JButton rules = new JButton("How to play");
-		JButton exit = new JButton("Leave game and logout");
-		this.setLayout(new GridLayout(3, 0));
+		returnGame.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		returnGame.setBounds(400, 200, 250, 55);
+		add(returnGame);
+		
+		JButton instructionsButton = new JButton("Instructions");
+		instructionsButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		instructionsButton.setBounds(400, 268, 250, 55);
+		add(instructionsButton);
+		
+		JButton exitButton = new JButton("Logout");
+		exitButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		exitButton.setBounds(400, 336, 250, 55);
+		add(exitButton);
+		
 		returnGame.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -63,19 +90,19 @@ public class PausePanel extends JPanel {
 				swingUI.setInterface(Interface.IN_GAME);
 			}
 		});
-		rules.addActionListener(new ActionListener() {
+		instructionsButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				swingUI.playButtonSound();
 				swingUI.setInterface(Interface.RULES);
 			}
 		});
-		exit.addActionListener(new ActionListener() {
+		exitButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				swingUI.playButtonSound();
 				int confirmed = JOptionPane.showConfirmDialog(null, 
-						"Are you sure you want to exit the game?", "Exit Trouble Message",
+						"Are you sure you want to exit the game? You will leave the game and disrupt play for others", "Exit Trouble Message",
 						JOptionPane.YES_NO_OPTION);
 					if (confirmed == JOptionPane.YES_OPTION)
 						swingUI.send(CommunicationHandler.LOGOUT_REQUEST);
@@ -99,35 +126,35 @@ public class PausePanel extends JPanel {
 			public void mouseClicked(MouseEvent arg0) {}
 		});
 		
-		rules.addMouseListener(new MouseListener() {			
+		instructionsButton.addMouseListener(new MouseListener() {			
 			@Override
 			public void mouseReleased(MouseEvent arg0) {}		   
 			@Override
 			public void mousePressed(MouseEvent arg0) {}			
 			@Override
 			public void mouseExited(MouseEvent arg0) { 
-				rules.setIcon(imgIcon1);
+				instructionsButton.setIcon(imgIcon1);
 			}		   
 			@Override
 			public void mouseEntered(MouseEvent arg0) {
-				rules.setIcon(imgIcon2);
+				instructionsButton.setIcon(imgIcon2);
 			}		   
 			@Override
 			public void mouseClicked(MouseEvent arg0) {}
 		});
 		
-		exit.addMouseListener(new MouseListener() {			
+		exitButton.addMouseListener(new MouseListener() {			
 			@Override
 			public void mouseReleased(MouseEvent arg0) {}		   
 			@Override
 			public void mousePressed(MouseEvent arg0) {}			
 			@Override
 			public void mouseExited(MouseEvent arg0) { 
-				exit.setIcon(imgIcon1);
+				exitButton.setIcon(imgIcon1);
 			}		   
 			@Override
 			public void mouseEntered(MouseEvent arg0) {
-				exit.setIcon(imgIcon2);
+				exitButton.setIcon(imgIcon2);
 			}		   
 			@Override
 			public void mouseClicked(MouseEvent arg0) {}
@@ -138,19 +165,19 @@ public class PausePanel extends JPanel {
 		returnGame.setBorderPainted(false);
 		returnGame.setFont(new Font("Arial", Font.PLAIN, 30));
 		
-		rules.setIcon(imgIcon1);
-		rules.setHorizontalTextPosition(SwingConstants.CENTER);
-		rules.setBorderPainted(false);
-		rules.setFont(new Font("Arial", Font.PLAIN, 30));
+		instructionsButton.setIcon(imgIcon1);
+		instructionsButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		instructionsButton.setBorderPainted(false);
+		instructionsButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		
-		exit.setIcon(imgIcon1);
-		exit.setHorizontalTextPosition(SwingConstants.CENTER);
-		exit.setBorderPainted(false);
-		exit.setFont(new Font("Arial", Font.PLAIN, 30));
+		exitButton.setIcon(imgIcon1);
+		exitButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		exitButton.setBorderPainted(false);
+		exitButton.setFont(new Font("Arial", Font.PLAIN, 30));
 		
 		this.add(returnGame);
-		this.add(rules);
-		this.add(exit);
+		this.add(instructionsButton);
+		this.add(exitButton);
 		this.validate();
 		this.repaint();
 	}
@@ -170,4 +197,11 @@ public class PausePanel extends JPanel {
 		this.swingUI = swingUI;
 	}
 
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	
+		// Draw the background image.
+		g.drawImage(backgroundImage, 0, 0, this);
+	}
 }
